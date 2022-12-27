2007bcafafb41444464d22ff9db1e04089dcc06d
= <=>
2022-12-27 16:23:21 +0700

    lab10_2_completed

--- lab10_2.cpp
+++ lab10_2.cpp
@@ -24,24 +24,3 @@ void printO(int N , int M )
 }
 
 
-
-int main(){
-	
-	printO(2,2);
-	cout << "\n";
-
-	printO(3,5);
-	cout << "\n";	
-	
-	printO(5,3);
-	cout << "\n";	
-	
-	printO(0,3);
-	cout << "\n";	
-	
-	printO(7,-1);
-	cout << "\n";	
-
-	return 0;
-}
-
